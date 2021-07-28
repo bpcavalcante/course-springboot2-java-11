c1ece4eaaec2b1ae51371f095f0bf39806628e55
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-07-27 21:31:27 -0300

    Service, component

--- src/main/java/com/profetasolucoes/course/repositories/UserRepository.java
+++ src/main/java/com/profetasolucoes/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.profetasolucoes.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.profetasolucoes.course.entites.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
