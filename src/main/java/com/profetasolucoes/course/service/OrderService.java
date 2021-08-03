48b0ba6116de81a580f1c8c91dbb711b3f0648b8
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-08-03 20:01:10 -0300

    OrderStatus Enum

---
+++ src/main/java/com/profetasolucoes/course/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.profetasolucoes.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.profetasolucoes.course.entites.Order;
+import com.profetasolucoes.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
