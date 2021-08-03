3db9a0bca0bc637ec1e1964ddd0da9a9dcab45c8
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-08-03 20:15:30 -0300

    Category

---
+++ src/main/java/com/profetasolucoes/course/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.profetasolucoes.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.profetasolucoes.course.entites.Category;
+import com.profetasolucoes.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
