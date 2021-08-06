0cb47ab4bf86006c849c278d1cef9f37ef333b88
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-08-05 22:07:45 -0300

    OrderItem , many-to-many parte 2

--- src/main/java/com/profetasolucoes/course/config/TestConfig.java
+++ src/main/java/com/profetasolucoes/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.profetasolucoes.course.entites.Category;
 import com.profetasolucoes.course.entites.Order;
+import com.profetasolucoes.course.entites.OrderItem;
 import com.profetasolucoes.course.entites.Product;
 import com.profetasolucoes.course.entites.User;
 import com.profetasolucoes.course.entites.enums.OrderStatus;
 import com.profetasolucoes.course.repositories.CategoryRepository;
+import com.profetasolucoes.course.repositories.OrderItemRepository;
 import com.profetasolucoes.course.repositories.OrderRepository;
 import com.profetasolucoes.course.repositories.ProductRepository;
 import com.profetasolucoes.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -46,16 +51,23 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-		
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		
+		
 
 		Category cat1 = new Category(null, "Eletronics");
 		Category cat2 = new Category(null, "Books");
@@ -74,7 +86,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 
